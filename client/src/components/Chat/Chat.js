57d28e9c55d8dde0eb47910cc78f81b917a4e360
Gokul <gokulramalingam1@gmail.com>
2019-10-30 18:20:27 +0530

    update in chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React,{ useState,useEffect } from 'react'
+import querystring from 'query-string'
+import io from 'socket.io-client'
 
-const Chat = () =>{
-    return(
-        <h1>Chat</h1>
-    )
+let socket;
+
+const Chat = ({location}) =>{
+    const [name, setName] = useState(' ');
+    const [room, setRoom] = useState(' ');
+    const ENDPOINT = 'localhost:2000';
+
+   useEffect(() => {
+       const {name,room} = querystring.parse(location.search);
+
+      socket = io(ENDPOINT);
+
+       setName(name);
+       setRoom(room);
+
+
+       socket.emit('join',{ name,room });
+
+   },[ENDPOINT,location.search])
+
+   return(
+    <h1>Chat</h1>
+)
 }
 
 export default Chat;
