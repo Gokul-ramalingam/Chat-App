729bc1776da2eb233f7a145f1fc2571f4c5d6b84
Gokul <gokulramalingam1@gmail.com>
2019-10-31 01:54:59 +0530

    Update in route

--- server/router/route.js
+++ server/router/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('Message server is up and running')
+    res.send('server is up and running')
 })
 
 
