f2cc0e8ad5acb854d2ede8cc00decda905f2d0ca
Gokul <gokulramalingam1@gmail.com>
2019-10-30 14:40:40 +0530

    Update in server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const router = require('./router/route')
+
+const port = process.env.PORT || 2000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+
+io.on('connection', (socket) =>{
+    console.log("New connection has been established!!!");
+
+    socket.on('disconnect', () =>{
+        console.log("Disconnection has been established!!!");
+    })
+})
+
+
+
+app.use(router);
+
+server.listen(port,()=>{
+    console.log("Server is running on port "+port);
+})
