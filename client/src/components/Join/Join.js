f6356251e1c8885f39ff458d4eec0ffdfbd84665
Gokul <gokulramalingam1@gmail.com>
2019-10-30 14:50:27 +0530

    Update in component

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,9 +10,9 @@ const Join = () =>{
         <div className="joinOuterContainer">
         <div className="joinInnerContainer">
         <h1 className="heading">Join</h1>
-        <div><input placeholder="" className="joinInput" type="text" onChange= {}/></div>
-        <div><input placeholder="" className="joinInput mt-20" type="text" onChange= {}/></div>
-        <Link>
+        <div><input placeholder="" className="joinInput" type="text" onChange= {(event) => setName(event.target.value)}/></div>
+        <div><input placeholder="" className="joinInput mt-20" type="text" onChange= {(event) => setRoom(event.target.value)}/></div>
+        <Link onClick={event => (!name || !room ? event.preventDefault():null)} to={`/chat?name=${name}&room=${room}`}>
         <button className="button mt-20" type="submit">Sign In</button>
         </Link>
         </div>
